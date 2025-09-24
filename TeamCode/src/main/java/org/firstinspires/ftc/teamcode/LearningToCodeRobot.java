45f864dbf6932acff261cd616e3a8bb934f05d5d
Alex1234560 <alejandrochoarteaga@gmail.com>
2025-09-24 10:50:48 -0700

    Create LearningToCodeRobot.java
    Created first ever java class, and learnt some coding on this .

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LearningToCodeRobot.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled; // Remove this too
+@Disabled //Remove If You wAnt this to show up on the Driver Station
+@TeleOp
+public class LearningToCodeRobot extends OpMode {
+
+    @Override
+    public void init() {
+        telemetry.addData("Hello","World");
+
+
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
+
+
+
