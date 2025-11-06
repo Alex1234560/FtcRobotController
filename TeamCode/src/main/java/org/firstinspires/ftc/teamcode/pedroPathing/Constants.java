753e5980994d339eb58633b88ea00e23c80441fc
Alex1234560 <alejandrochoarteaga@gmail.com>
2025-11-05 16:10:29 -0800

    Update Constants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -13,12 +13,16 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 public class Constants {
 
     public static FollowerConstants followerConstants = new FollowerConstants()
-        .mass(3);
+        .mass(3)
+        .forwardZeroPowerAcceleration(-87.709)
+        .lateralZeroPowerAcceleration(-152.68700);
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
     public static MecanumConstants driveConstants = new MecanumConstants()
             .maxPower(1)
+            .xVelocity(84.7084)
+            .yVelocity(60.2149)
             .leftRearMotorName("leftBack")
             .leftFrontMotorName("leftFront")
             .rightFrontMotorName("rightFront")
@@ -26,7 +30,10 @@ public class Constants {
             .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
             .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD);
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            //.useBrakeModeInTeleOp(false)
+
+            ;
 
     public static DriveEncoderConstants localizerConstants = new DriveEncoderConstants()
             .robotWidth(16.5)
@@ -38,11 +45,18 @@ public class Constants {
             .leftFrontEncoderDirection(Encoder.REVERSE)
             .leftRearEncoderDirection(Encoder.REVERSE)
             .rightFrontEncoderDirection(Encoder.FORWARD)
-            .rightRearEncoderDirection(Encoder.FORWARD);
+            .rightRearEncoderDirection(Encoder.FORWARD)
+            .forwardTicksToInches(0.00812112117)
+            .strafeTicksToInches(.0061745552)
+            .turnTicksToInches(.0124227)
+
+            ;
 
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
+                .pathConstraints(pathConstraints)
+                .mecanumDrivetrain(driveConstants) // This is the corrected line
                 .pathConstraints(pathConstraints)
                 .driveEncoderLocalizer(localizerConstants)
                 .build();
